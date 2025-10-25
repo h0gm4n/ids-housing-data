d675dc61d39ae0770ada320357d65afa8f01ca76
h0gm4n <atte.hogman@gmail.com>
2025-10-25 16:34:22 +0300

    modify script

--- housingdata/setup.sh
+++ housingdata/setup.sh
@@ -4,7 +4,7 @@ echo "Installing frontend (npm) dependencies..."
 npm install
 
 echo "Installing backend (Python) dependencies..."
-cd housingdata/src/backend
+cd src/backend
 python3 -m venv .venv
 source .venv/bin/activate
 pip install flask flask-cors
